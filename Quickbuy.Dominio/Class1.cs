752d6ab2f77d796886d6aefc288de4825eccb0a2
Luis Claudio Pimenta Riccio de <luis_claudio_pimenta@hotmail.com>
2021-05-10 17:35:07 -0300

    Revert "QuickBuy"
    This reverts commit d950a5cf70e99b97cfa7f91d261caf08d0f6b414.

--- Quickbuy.Dominio/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Quickbuy.Dominio
-{
-    public class Class1
-    {
-    }
-}
--- Quickbuy.Repositorio/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Quickbuy.Repositorio
-{
-    public class Class1
-    {
-    }
-}
